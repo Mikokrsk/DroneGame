6a5f83ec0c2397bb563c16f6df9016f7012a384d
Mykola Krasko <mikokrsk@gmail.com>
2024-05-23 21:35:45 +0300

    Added Virtual Cameras
    + Added MainDroneCamera
    + Added SecondDroneCamera

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,26 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] private float _cameraRotationSpeed = 1;
     [SerializeField] private float smoothSpeed = 5f;
     [SerializeField] private float targetRotationX;
-
-   // [SerializeField] private Transform _droneTransform;
+    [SerializeField] private GameObject _droneCameraBody;
+    [SerializeField] private CinemachineVirtualCamera _droneMainCamera;
+    [SerializeField] private CinemachineVirtualCamera _droneSecondCamera;
+    [SerializeField] private int _hightCameraPriority = 1;
+    [SerializeField] private int _lowCameraPriority = 0;
+    // [SerializeField] private Transform _droneTransform;
     private void Update()
     {
         CameraRotation();
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SwitchCamera();
+        }
     }
 
     private void LateUpdate()
     {
-      //  transform.position = _droneTransform.position;
-      //  transform.eulerAngles = new Vector3(transform.eulerAngles.x, _droneTransform.eulerAngles.y, 0);
+        //  transform.position = _droneTransform.position;
+        //  transform.eulerAngles = new Vector3(transform.eulerAngles.x, _droneTransform.eulerAngles.y, 0);
     }
 
     public void CameraRotation()
@@ -32,12 +42,26 @@ public class FollowCamera : MonoBehaviour
             direction = 1;
         }
 
-        targetRotationX = Mathf.Clamp(transform.localEulerAngles.x + direction * _cameraRotationSpeed, 0f, 90f);
+        targetRotationX = Mathf.Clamp(_droneCameraBody.transform.localEulerAngles.x + direction * _cameraRotationSpeed, 0f, 90f);
 
         float newRotationX = Mathf.Lerp(transform.localEulerAngles.x, targetRotationX, _cameraRotationSpeed * Time.deltaTime);
 
-        transform.localEulerAngles = new Vector3(newRotationX, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        _droneCameraBody.transform.localEulerAngles = new Vector3(newRotationX, _droneCameraBody.transform.localEulerAngles.y, _droneCameraBody.transform.localEulerAngles.z);
 
+    }
+
+    private void SwitchCamera()
+    {
+        if (_droneMainCamera.Priority > _droneSecondCamera.Priority)
+        {
+            _droneSecondCamera.Priority = _hightCameraPriority;
+            _droneMainCamera.Priority = _lowCameraPriority;
+        }
+        else
+        {
+            _droneMainCamera.Priority = _hightCameraPriority;
+            _droneSecondCamera.Priority = _lowCameraPriority;
+        }
     }
 
 }
