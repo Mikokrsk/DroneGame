48e9d222fac70acd2d83c954a2fda4141c87ae11
Mykola Krasko <mikokrsk@gmail.com>
2024-05-27 13:00:14 +0300

    Added Drone Animations
    + Added PropellerRotation Animation
    + Added MoveRotation method

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     [SerializeField] private float _cameraRotationSpeed = 1;
     [SerializeField] private float smoothSpeed = 5f;
@@ -13,7 +13,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera _droneSecondCamera;
     [SerializeField] private int _hightCameraPriority = 1;
     [SerializeField] private int _lowCameraPriority = 0;
-    // [SerializeField] private Transform _droneTransform;
+
     private void Update()
     {
         CameraRotation();
@@ -22,12 +22,6 @@ public class FollowCamera : MonoBehaviour
         {
             SwitchCamera();
         }
-    }
-
-    private void LateUpdate()
-    {
-        //  transform.position = _droneTransform.position;
-        //  transform.eulerAngles = new Vector3(transform.eulerAngles.x, _droneTransform.eulerAngles.y, 0);
     }
 
     public void CameraRotation()
@@ -42,12 +36,17 @@ public class FollowCamera : MonoBehaviour
             direction = 1;
         }
 
-        targetRotationX = Mathf.Clamp(_droneCameraBody.transform.localEulerAngles.x + direction * _cameraRotationSpeed, 0f, 90f);
+        if (direction != 0)
+        {
+            float currentRotationX = _droneCameraBody.transform.localEulerAngles.x;
+            if (currentRotationX > 180f) currentRotationX -= 360f;
 
-        float newRotationX = Mathf.Lerp(transform.localEulerAngles.x, targetRotationX, _cameraRotationSpeed * Time.deltaTime);
+            targetRotationX = Mathf.Clamp(currentRotationX + direction * _cameraRotationSpeed, 0f, 90f);
 
-        _droneCameraBody.transform.localEulerAngles = new Vector3(newRotationX, _droneCameraBody.transform.localEulerAngles.y, _droneCameraBody.transform.localEulerAngles.z);
+            float newRotationX = Mathf.Lerp(currentRotationX, targetRotationX, smoothSpeed * Time.deltaTime);
 
+            _droneCameraBody.transform.localEulerAngles = new Vector3(newRotationX, _droneCameraBody.transform.localEulerAngles.y, _droneCameraBody.transform.localEulerAngles.z);
+        }
     }
 
     private void SwitchCamera()
@@ -63,5 +62,4 @@ public class FollowCamera : MonoBehaviour
             _droneSecondCamera.Priority = _lowCameraPriority;
         }
     }
-
 }
--- Assets/Scripts/Drone/DroneMoveContoller.cs
+++ Assets/Scripts/Drone/DroneMoveContoller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,20 @@ public class DroneMoveContoller : MonoBehaviour
     //  private Rigidbody rb;
 
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private FollowCamera _followCamera;
+    [SerializeField] private CameraController _followCamera;
     [SerializeField] private float _horizontalSpeed = 1;
     [SerializeField] private float _verticalSpeed = 1;
+    [SerializeField] private float _acceleration = 2f;
+    [SerializeField] private float _deceleration = 2f;
+    [SerializeField] private float _maxTiltAngle = 20f;
+    [SerializeField] private float _rotationSpeed = 2f;
     [SerializeField] private float _droneRotationSpeed = 1;
-
+    [SerializeField] private Animator _droneAnimator;
+    [SerializeField] private Vector3 moveDirection;
     private void Start()
     {
         //   rb = GetComponent<Rigidbody>();
+        moveDirection = Vector3.zero;
     }
 
     void Update()
@@ -22,44 +29,51 @@ public class DroneMoveContoller : MonoBehaviour
         HorizontalMove();
         VerticalMove();
         DroneRotation();
+        //   RotateDrone();
     }
 
     private void HorizontalMove()
     {
-        Vector3 direction = Vector3.zero;
+        float tiltX = 0f;
+        float tiltZ = 0f;
+        Vector3 targetDirection = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
-            direction += transform.forward;
+            targetDirection += transform.forward;
+            tiltX = _maxTiltAngle;
         }
-        else
+        if (Input.GetKey(KeyCode.S))
         {
-            if (Input.GetKey(KeyCode.S))
-            {
-                direction += -transform.forward;
-            }
+            targetDirection += -transform.forward;
+            tiltX = -_maxTiltAngle;
         }
-
         if (Input.GetKey(KeyCode.A))
         {
-            direction += -transform.right;
+            targetDirection += -transform.right;
+            tiltZ = _maxTiltAngle;
         }
-        else
+        if (Input.GetKey(KeyCode.D))
         {
-            if (Input.GetKey(KeyCode.D))
-            {
-                direction += transform.right;
-            }
+            targetDirection += transform.right;
+            tiltZ = -_maxTiltAngle;
         }
 
-        if (direction.magnitude > 1f)
+        targetDirection = targetDirection.normalized;
+
+        moveDirection = Vector3.Lerp(moveDirection, targetDirection, _acceleration * Time.deltaTime);
+
+        if (targetDirection == Vector3.zero)
         {
-            direction = direction.normalized;
+            moveDirection = Vector3.Lerp(moveDirection, Vector3.zero, _deceleration * Time.deltaTime);
         }
 
-        // rb.AddForce(direction * _horizontalForce, ForceMode.Force);       
-        characterController.Move(direction * Time.deltaTime * _horizontalSpeed);
+        characterController.Move(moveDirection * Time.deltaTime * _horizontalSpeed);
+
+        Quaternion targetRotation = Quaternion.Euler(tiltX, transform.eulerAngles.y, tiltZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
     }
+
     private void VerticalMove()
     {
         Vector3 direction = Vector3.zero;
@@ -76,26 +90,26 @@ public class DroneMoveContoller : MonoBehaviour
 
         characterController.Move(direction * Time.deltaTime * _verticalSpeed);
     }
+
     private void DroneRotation()
     {
         var direction = 0;
         if (Input.GetKey(KeyCode.Z))
         {
             direction = -1;
+            //   transform.Rotate(0, -_droneRotationSpeed * Time.deltaTime, 0);
         }
-        else
+
+        if (Input.GetKey(KeyCode.X))
         {
-            if (Input.GetKey(KeyCode.X))
-            {
-                direction = 1;
-            }
+
+            direction = 1;
+            //  transform.Rotate(0, _droneRotationSpeed * Time.deltaTime, 0);
         }
-
-
-
+        //  _droneAnimator.SetFloat("DroneRotation", direction);
         // transform.localEulerAngles = new Vector3(_droneRotationSpeed * direction * Time.deltaTime, transform.localEulerAngles.y, transform.localEulerAngles.z);
 
-        transform.Rotate(0, direction * _droneRotationSpeed * Time.deltaTime,0);
+        transform.Rotate(0, direction * _droneRotationSpeed * Time.deltaTime, 0);
     }
 
 }
