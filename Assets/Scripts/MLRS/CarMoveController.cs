c4671187c672831e3a23f733070f85aac8156aba
Mykola Krasko <mikokrsk@gmail.com>
2024-07-10 16:13:51 +0300

    Added VehicleMoveController
    + Added abstract VehicleMoveController
    + Added WheeledVehicleMoveController
    + Added CarMoveController
    + Added CarNavigationSystem

--- Assets/Scripts/MLRS/CarMoveController.cs
+++ Assets/Scripts/MLRS/CarMoveController.cs
@@ -5,139 +5,65 @@ using UnityEngine;
 
 public class CarMoveController : MonoBehaviour
 {
-    [SerializeField] private bool _isActive;
-    [SerializeField] private Wheel[] wheels;
-
-    [SerializeField] private Rigidbody rb;
-
-    [SerializeField] private float maxMotorTorque = 2000;
-    [SerializeField] private float maxBrakeTorque = 2000;
-    [SerializeField] private float maxSpeed = 20;
-    [SerializeField] private float maxSteeringAngle = 30;
-
-    [SerializeField] private float currentMotorTorque = 0;
-    [SerializeField] private float currentBrakeTorque = 0;
-    [SerializeField] private float currentSpeed = 0;
-    [SerializeField] private float currentSteeringRange = 30;
-
-    [SerializeField] private float centreOfGravityOffset = -1f;
-
+    [SerializeField] public WheeledVehicleMoveController wheeledVehicleMoveController;
     [SerializeField] private KeyCode moveFWD = KeyCode.W;
     [SerializeField] private KeyCode moveRight = KeyCode.D;
     [SerializeField] private KeyCode moveLeft = KeyCode.A;
     [SerializeField] private KeyCode moveBack = KeyCode.S;
     [SerializeField] private KeyCode stopMove = KeyCode.Space;
 
-    void Start()
+    protected void Update()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.centerOfMass += Vector3.up * centreOfGravityOffset;
-
-        wheels = GetComponentsInChildren<Wheel>();
-    }
-
-    private void FixedUpdate()
-    {
-        if (_isActive)
+        if (wheeledVehicleMoveController.isActive)
         {
-            UpdateSpeedometer();
             CheckSteeringInput();
             CheckGasPedalInput();
-        }
-        ApplyBrakes();
-    }
-
-    public void SetActive(bool active)
-    {
-        _isActive = active;
-    }
-    public bool GetActive()
-    {
-        return _isActive;
-    }
-
-    public float GetCurrentSpeed()
-    {
-        return currentSpeed;
-    }
-
-    private void UpdateSpeedometer()
-    {
-        float forwardSpeed = Vector3.Dot(transform.forward, rb.velocity);
-        currentSpeed = (int)(forwardSpeed * 3.6f); // Convert m/s to km/h
-    }
-
-    private void ApplyBrakes()
-    {
-        if (Input.GetKey(stopMove) || (currentMotorTorque == 0 && currentSpeed == 0))
-        {
-            currentBrakeTorque = maxBrakeTorque;
-        }
-        else
-        {
-            currentBrakeTorque = 0f;
-        }
-
-        foreach (var wheel in wheels)
-        {
-            wheel.WheelCollider.brakeTorque = currentBrakeTorque;
+            CheckBrakesInput();
         }
     }
 
     private void CheckGasPedalInput()
     {
-        float direction = 0f;
-
-        if (Input.GetKey(moveFWD) && currentSpeed <= maxSpeed)
+        if (Input.GetKey(moveFWD))
         {
-            direction = 1;
-            currentMotorTorque = maxMotorTorque;
+            wheeledVehicleMoveController.MoveForward();
         }
-        else if (Input.GetKey(moveBack) && currentSpeed >= -maxSpeed)
+        else if (Input.GetKey(moveBack))
         {
-            direction = -1;
-            currentMotorTorque = maxMotorTorque;
+            wheeledVehicleMoveController.MoveBackward();
         }
         else
         {
-            currentMotorTorque = 0f;
+            wheeledVehicleMoveController.ResetMotorTorque();
         }
-
-        SetWheelTorque(direction);
     }
 
-    private void SetWheelTorque(float direction)
+    private void CheckBrakesInput()
     {
-        foreach (var wheel in wheels)
+        if (Input.GetKey(stopMove))
         {
-            wheel.WheelCollider.motorTorque = maxMotorTorque * direction;
+            wheeledVehicleMoveController.ApplyBrakes(1f);
         }
     }
 
     private void CheckSteeringInput()
     {
-        float steerAngle = 0f;
-
         if (Input.GetKey(moveRight))
         {
-            steerAngle = maxSteeringAngle;
+            wheeledVehicleMoveController.TurnRight();
         }
         else if (Input.GetKey(moveLeft))
         {
-            steerAngle = -maxSteeringAngle;
+            wheeledVehicleMoveController.TurnLeft();
         }
-
-        SetWheelsSteerAngle(steerAngle);
+        else
+        {
+            wheeledVehicleMoveController.ResetSteeringAngle();
+        }
     }
 
-    private void SetWheelsSteerAngle(float steerAngle)
+    public float GetCurrentSpeed()
     {
-        foreach (var wheel in wheels)
-        {
-            if (wheel.steerable)
-            {
-                wheel.WheelCollider.steerAngle = steerAngle;
-            }
-        }
+        return wheeledVehicleMoveController.GetCurrentSpeed();
     }
 }
